677a806f7d972f5e6c3bc6a7f145a705068939a1
Melike Ünsaldı <100564433+Melikensld@users.noreply.github.com>
2022-03-07 11:18:28 +0300

    updated ArtikYilHesaplama.java

---
+++ src/ArtikYilHesaplama.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArtikYilHesaplama {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        int year;
+
+        System.out.print("Yıl Giriniz : ");
+        year = input.nextInt();
+
+        if ((year % 4 == 0) && (year % 400 != 0 ) && (year % 100 != 0)) {
+            System.out.print(year + " bir artık yıldır ! ");
+
+        }else if ((year % 400 == 0) && (year % 100 == 0) && (year % 4 == 0) ) {
+
+            System.out.print(year + " bir artık yıldır ! ");
+
+        } else {
+            System.out.println(year + " bir artık yıl değildir ! ");
+        }
+
+
+    }
+}
