68946b50fe339cc2e870f904869fdcfe3092cfc3
Melike Ünsaldı <100564433+Melikensld@users.noreply.github.com>
2022-06-26 17:21:28 +0300

    updated Elmas.java

---
+++ src/Elmas.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+/*
+Java'da döngüler kullanılarak yıldızlar ile elmas yapımı
+ */
+public class Elmas {
+
+    public static void main(String[] args) {
+        int n,a,b,c;
+        Scanner inp = new Scanner(System.in);
+        System.out.print(" Bir Sayı gırınız : ");
+        n = inp.nextInt();
+        for ( a = 1; a <= n; a++) {
+            for ( b = 1; b <= (n - a); b++) {
+                System.out.print(" ");
+
+            }
+            for ( c = 1; c <= ( 2* a - 1 ); c++) {
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+
+        }
+        for ( a = 1; a < n; a++) {
+            for ( b = 1; b <= a; b++) {
+                System.out.print(" ");
+
+            }
+            for ( c = 1; c <= ( 2 * n - (2 * a + 1) ); c++) {
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
