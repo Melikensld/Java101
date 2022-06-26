a949f8cac472545dd10d3e1e455b4067d63a255c
Melike Ünsaldı <100564433+Melikensld@users.noreply.github.com>
2022-06-26 17:08:34 +0300

    updated UsluSayiHesaplayanProgram.java

---
+++ src/UsluSayiHesaplayanProgram.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UsluSayiHesaplayanProgram {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Base : ");
+        int x = sc.nextInt();
+        System.out.print("Exponent : ");
+        int y = sc.nextInt();
+
+        int total = 1;
+
+        for (int i=1;i<=y;i++) {
+            total*=x;
+            //System.out.println(total);
+        }
+        System.out.println("Total : "+ total);
+    }
+}
